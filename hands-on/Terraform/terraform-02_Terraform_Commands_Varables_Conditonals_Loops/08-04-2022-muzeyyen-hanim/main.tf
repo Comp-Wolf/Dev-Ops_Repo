19f350de70657f88fd2f766b10aec8df4cc7113b
Comp-Wolf <alierdogan38@gmail.com>
2022-04-08 02:24:10 +0300

    update

--- hands-on/Terraform/terraform-02_Terraform_Commands_Varables_Conditonals_Loops/08-04-2022-muzeyyen-hanim/main.tf
+++ hands-on/Terraform/terraform-02_Terraform_Commands_Varables_Conditonals_Loops/08-04-2022-muzeyyen-hanim/main.tf
@@ -2,6 +2,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "name" {
+resource "aws_instance" "comp-wolf" {
+  ami="ami-0ed9277fb7eb570c9"
+  instance_type="t2.micro"
+  key_name="comp-wolf"
+  aws_security_group = "${aws_secount_group.SG1.Id}"
+}
+
+resource "aws_security_group" "SG1" {
+    ingress = [ {
+      cidr_blocks = [ "0.0.0.0/0" ]
+      from_port = 22
+      protocol = "SSH"
+      to_port = 22
+    } ]
   
 }
