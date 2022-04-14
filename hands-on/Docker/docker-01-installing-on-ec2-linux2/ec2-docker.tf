524e9cc6f9b3ecc3fb7de44d947f5f4393a0700c
Comp-Wolf <alierdogan38@gmail.com>
2022-04-14 17:38:02 +0300

    update

--- hands-on/Docker/docker-01-installing-on-ec2-linux2/ec2-docker.tf
+++ hands-on/Docker/docker-01-installing-on-ec2-linux2/ec2-docker.tf
@@ -63,7 +63,7 @@ resource "aws_instance" "tf-ec2" {
               systemctl enable docker
               usermod -a -G docker ec2-user
               # install docker-compose
-              curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" \
+              sudo
               -o /usr/local/bin/docker-compose
               chmod +x /usr/local/bin/docker-compose
 	            EOF
