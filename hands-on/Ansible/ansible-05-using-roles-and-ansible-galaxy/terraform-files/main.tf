f17ab9797681381d0453aeadace1ea3cbc916203
Comp-Wolf <alierdogan38@gmail.com>
2022-05-21 00:55:02 +0300

    update

--- hands-on/Ansible/ansible-05-using-roles-and-ansible-galaxy/terraform-files/main.tf
+++ hands-on/Ansible/ansible-05-using-roles-and-ansible-galaxy/terraform-files/main.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "4.14.0"
     }
   }
 }
@@ -63,7 +63,7 @@ resource "null_resource" "config" {
     host = aws_instance.nodes[0].public_ip
     type = "ssh"
     user = "ec2-user"
-    private_key = file("~/.ssh/${var.mykeypem}")
+    private_key = file("E:/Clarusway/DERSLER/Dev-Ops_Repo/${var.mykeypem}")
     # Do not forget to define your key file path correctly!
   }
 
@@ -74,7 +74,7 @@ resource "null_resource" "config" {
 
   provisioner "file" {
     # Do not forget to define your key file path correctly!
-    source = "~/.ssh/${var.mykeypem}"
+    source = "E:/Clarusway/DERSLER/Dev-Ops_Repo/${var.mykeypem}"
     destination = "/home/ec2-user/${var.mykeypem}"
   }
 
