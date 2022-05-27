7ddfe14fb870c51b438c11d2d4d5878dcb3c1d4f
Comp-Wolf <alierdogan38@gmail.com>
2022-05-28 02:55:52 +0300

    update

--- projects/207-Ansible-publish-website-postgresql-nodejs-react/terraform-files/main.tf
+++ projects/207-Ansible-publish-website-postgresql-nodejs-react/terraform-files/main.tf
@@ -19,26 +19,26 @@ provider "aws" {
 }
 
 resource "aws_instance" "control_node" {
-  ami = var.myami
-  instance_type = var.controlinstancetype
-  key_name = var.mykey
-  iam_instance_profile = aws_iam_instance_profile.ec2full.name
+  ami                    = var.myami
+  instance_type          = var.controlinstancetype
+  key_name               = var.mykey
+  iam_instance_profile   = aws_iam_instance_profile.ec2full.name
   vpc_security_group_ids = [aws_security_group.tf-sec-gr.id]
   tags = {
-    Name = "ansible_control"
+    Name  = "ansible_control"
     stack = "ansible_project"
   }
 }
 
 resource "aws_instance" "nodes" {
-  ami = var.myami
-  instance_type = var.instancetype
-  count = var.num
-  key_name = var.mykey
+  ami                    = var.myami
+  instance_type          = var.instancetype
+  count                  = var.num
+  key_name               = var.mykey
   vpc_security_group_ids = [aws_security_group.tf-sec-gr.id]
   tags = {
-    Name = "ansible_${element(var.tags, count.index )}"
-    stack = "ansible_project"
+    Name        = "ansible_${element(var.tags, count.index)}"
+    stack       = "ansible_project"
     environment = "development"
   }
   user_data = file("userdata.sh")
@@ -110,26 +110,26 @@ resource "aws_security_group" "tf-sec-gr" {
 resource "null_resource" "config" {
   depends_on = [aws_instance.control_node]
   connection {
-    host = aws_instance.control_node.public_ip
-    type = "ssh"
-    user = "ec2-user"
+    host        = aws_instance.control_node.public_ip
+    type        = "ssh"
+    user        = "ec2-user"
     private_key = file("~/.ssh/${var.mykeypem}")
     # Do not forget to define your key file path correctly!
   }
 
   provisioner "file" {
-    source = "./ansible.cfg"
+    source      = "./ansible.cfg"
     destination = "/home/ec2-user/.ansible.cfg"
   }
 
   provisioner "file" {
-    source = "./inventory_aws_ec2.yml"
+    source      = "./inventory_aws_ec2.yml"
     destination = "/home/ec2-user/inventory_aws_ec2.yml"
   }
 
   provisioner "file" {
     # Do not forget to define your key file path correctly!
-    source = "~/.ssh/${var.mykeypem}"
+    source      = "~/.ssh/${var.mykeypem}"
     destination = "/home/ec2-user/${var.mykeypem}"
   }
 
