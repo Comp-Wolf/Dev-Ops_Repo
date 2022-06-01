d62e0c9b15f792c06d7fe0102efbd6e45abb8d86
Comp-Wolf <alierdogan38@gmail.com>
2022-06-01 20:47:14 +0300

    update

--- hands-on/ECR/ecr-01-elastic-container-registry-and-docker-images/ecr-ec2.tf
+++ hands-on/ECR/ecr-01-elastic-container-registry-and-docker-images/ecr-ec2.tf
@@ -101,5 +101,5 @@ output "ec2-public-ip" {
 }
 
 output "ssh-connection" {
-  value = "ssh -i ~/.ssh/mk.pem ec2-user@${aws_instance.ecr-instance.public_ip}"
+  value = "ssh -i ~/.ssh/comp-wolf.pem ec2-user@${aws_instance.ecr-instance.public_ip}"
 }
