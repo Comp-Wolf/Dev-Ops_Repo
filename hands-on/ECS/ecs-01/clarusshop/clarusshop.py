04074e1eedf5694eb4fa2a68dabdac533e2dd424
Comp-Wolf <alierdogan38@gmail.com>
2022-06-02 19:12:37 +0300

    update

--- hands-on/ECS/ecs-01/clarusshop/clarusshop.py
+++ hands-on/ECS/ecs-01/clarusshop/clarusshop.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def storefront():
-    return '<h1>Welcome to clarusshop!</h1><h2>/account</h2><h2>/inventory</h2><h2>/shipping</h2>'
+    return '<h1>Merhaba to clarusshop! Group_____NIGHT-FIYHTER_____</h1><h2>/account</h2><h2>/inventory</h2><h2>/shipping</h2>'
 
 @app.route('/account')
 def account():
